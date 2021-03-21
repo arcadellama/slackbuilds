4e745a045de82668829272f8ddfc03ae5a0ce5f7
Justin <61114996+arcadellama@users.noreply.github.com>
2021-03-21 10:29:54 -0500

    Update doinst.sh

--- nzbget/doinst.sh
+++ nzbget/doinst.sh
@@ -11,16 +11,5 @@ config() {
   # Otherwise, we leave the .new copy for the admin to consider...
 }
 
-preserve_perms() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  if [ -e $OLD ]; then
-    cp -a $OLD ${NEW}.incoming
-    cat $NEW > ${NEW}.incoming
-    mv ${NEW}.incoming $NEW
-  fi
-  config $NEW
-}
-
-preserve_perms etc/rc.d/rc.nzbget.new
-config etc/nzbget.conf.new
+config usr/share/nzbget/nzbget.conf.new
+config usr/share/nzbget/rc.nzbget.new
