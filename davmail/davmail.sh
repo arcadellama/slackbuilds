90bfd05b131334b757261a5fd752456c77c374cd
Justin <arcadellama@posteo.net>
2021-03-27 15:20:25 -0500

    cleanup

--- davmail/davmail.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-BASE=/usr/share/davmail
-for i in $BASE/lib/*; do export CLASSPATH=$CLASSPATH:$i; done
-exec /usr/lib64/java/jre/java -Xmx512M -Dsun.net.inetaddr.ttl=60 -cp $BASE/davmail.jar:$CLASSPATH davmail.DavGateway $1
